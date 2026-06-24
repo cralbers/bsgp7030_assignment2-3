876c37d916f704dad9aa55b692269a661554d5ac
Cora Albers <cralbers3@gmail.com>
2026-06-23 22:08:29 -0400

    r executable file

--- manual/linear_regression_r.r
+++ manual/linear_regression_r.r
@@ -1,21 +1,26 @@
 install.packages("ggplot2")
 library(ggplot2)
 
-# import csv dataset
-df <- read.csv("regression_data.csv")
 
+# Ask user for inputs and save to variables 
+filename <- readline(prompt = "Enter filename: ")
+x = readline(prompt = "Enter column name for x values: ")
+y = readline(prompt = "Enter column name for y values: ")
+
+# import csv dataset
+df <- read.csv(filename)
 
 # make a scatter plot of Salary (y) as a fx of YearsExperience (x)
-plot(df$YearsExperience, df$Salary, col="purple")
+plot(df$x, df$y, col="purple")
 
 # fit a linear regression model to the data (salary as a fx of years of experience)
-model <- lm(Salary ~ YearsExperience, data=df)
+model <- lm(y ~ x, data=df)
 
 
 # graph the raw data and the calculated regression line
 ggplot() +
-    geom_point(aes(x=df$YearsExperience, y=df$Salary), color='purple') +
-    geom_line(aes(x=df$YearsExperience, y=predict(model, newdata=df)), color='red') +
+    geom_point(aes(x=df$x, y=df$y), color='purple') +
+    geom_line(aes(x=df$x, y=predict(model, newdata=df)), color='red') +
     ggtitle("Salary vs Experience") +
     xlab("Years of Experience") +
     ylab("Salary") +
